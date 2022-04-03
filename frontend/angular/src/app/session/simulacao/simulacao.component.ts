dad7ff77549ca5bf934ca0a626c359105f14014d
Daniel <danielmartinsreis@gmail.com>
2022-04-03 07:53:37 -0300

    Atualizando formato de dados, preparando simulação e recebendo o resultado da simulação

--- frontend/angular/src/app/session/simulacao/simulacao.component.ts
+++ frontend/angular/src/app/session/simulacao/simulacao.component.ts
@@ -3,6 +3,8 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {first} from "rxjs/operators";
 import {TranslateService} from "@ngx-translate/core";
+import {RequestSimulacao, ReturnSimulacao} from "@app/session/sension.model";
+import {SessionService} from "@app/session/session.service";
 
 @Component({
   selector: "app-simulacao",
@@ -17,10 +19,14 @@ export class SimulacaoComponent implements OnInit {
   userid = null;
   parcelaVariavel = true;
   parcelaFixa = false;
+  resultadoSimulacao: ReturnSimulacao = null;
+  dadosIniciaisSimulacao = true;
+  dadosResultadoSimulacao = false;
 
   constructor(
     translate: TranslateService,
     private formBuilder: FormBuilder,
+    private sessionService: SessionService,
     private route: ActivatedRoute,
     private router: Router) {
 
@@ -52,6 +58,29 @@ export class SimulacaoComponent implements OnInit {
       return;
     }
 
+    let data: RequestSimulacao = {
+      id: this.userid,
+      amount_installment: this.f.numeroTotalPrestacoes.value,
+      amount_of_rest_installment: this.f.numeroPrestacoesRestantes.value,
+      actual_value_installment: this.f.valorParcelaAtual.value,
+      financed_value_without_fee: this.f.valorCreditoFinanciado.value,
+      type: this.parcelaVariavel ? 'v' : (this.parcelaFixa ? 'f' : '')
+    }
+
+    this.sessionService.cadastraSolicitacao(data)
+        .subscribe(
+            response => {
+              this.resultadoSimulacao = response;
+              if (this.resultadoSimulacao != null) {
+                this.dadosIniciaisSimulacao = false;
+                this.dadosResultadoSimulacao = true;
+              }
+            }, error => {
+              console.log("error", error);
+            }
+        );
+
+    // this.router.navigate([`/session/resultadosimulacao/${this.userid}`]);
   }
 
   changeParcela() {
