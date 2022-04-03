9c9006536ba8ca1bf06ca860b96c9c9ca5b7bcd1
Daniel <danielmartinsreis@gmail.com>
2022-04-03 07:50:36 -0300

    Criando modelo para simulação

--- frontend/angular/src/app/session/sension.model.ts
+++ frontend/angular/src/app/session/sension.model.ts
@@ -12,11 +12,22 @@ export interface ReturnCreateUser {
 }
 
 export interface RequestSimulacao {
-
+  id: string;
+  amount_installment: number;
+  amount_of_rest_installment: number;
+  actual_value_installment: number;
+  financed_value_without_fee: number;
+  type: string;
 }
 
 export interface ReturnSimulacao {
-
+  actual_tax: number,
+  actual_value_installment: number,
+  new_value_installment: number,
+  new_tax: number,
+  saved_value: number,
+  new_total_value: number,
+  id_offer: string
 }
 
 export interface RequestSimulacaoResultado {
